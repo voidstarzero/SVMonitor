6ad2edaa87466cfd21f5c9af17739e787cfdb99c
James Arcus <jimbo@ucc.asn.au>
2019-07-24 12:55:39 +0800

    Enable window titlebar (for now)

--- src/SFMLWindow.cpp
+++ src/SFMLWindow.cpp
@@ -10,7 +10,7 @@ SFMLWindow::SFMLWindow(unsigned pxWidth, unsigned pxHeight, const char* title, u
         3, 2, sf::ContextSettings::Core); // Selected OpenGL version (major, minor, profile)
     sf::VideoMode windowSize(pxWidth, pxHeight);
 
-    m_mainWindow.create(windowSize, title, sf::Style::None, windowSettings);
+    m_mainWindow.create(windowSize, title, sf::Style::Default, windowSettings);
     m_mainWindow.setFramerateLimit(60);
 
     glewInit();
